9cda41e54f4759378123ea0916312d94994e7a59
R3LI4NT <75953873+R3LI4NT@users.noreply.github.com>
2022-06-30 23:54:35 -0300

    Update hashcrack.py

--- hashcrack.py
+++ hashcrack.py
@@ -28,7 +28,7 @@ parse.add_argument("--sha3_384","--sha3-384",help="SHA3-384 hash")
 ########## hash --> SHA3-512
 parse.add_argument("--sha3_512","--sha3-512",help="SHA3-512 hash")
 ########## Wordlist
-parse.add_argument("-w","--wordlist",help="Wordlist", required=True)
+parse.add_argument("-w","--wordlist",help="Wordlist")
 parse.add_argument("-o", "--output",default="stdout",required=False, dest='output', help="Directs the output to a name of your choice")
 parse = parse.parse_args()
 
